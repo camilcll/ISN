31b2dd767381814447836901243983160593b708
camilcll <38221379+camilcll@users.noreply.github.com>
2018-05-02 17:37:33 +0200

    upload more files

---
+++ serialcommunication/sketch_mar22a/sketch_mar22a.ino
@@ -0,0 +1,33 @@
+
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial stationserial(7, 6, true); // (RX, TX) (pin Rx station, pin Tx station)
+byte c;
+
+void setup()
+{
+    // Ouvre la voie série avec l'ordinateur
+    Serial.begin(9600);  
+    // Ouvre la voie série avec la station
+    stationserial.begin(4800);
+    
+}
+
+void loop()
+{
+    
+    if (Serial.available()) {
+       stationserial.write(Serial.read());   
+    }
+    
+    if (stationserial.available()) {
+       c=stationserial.read();
+        Serial.print(c);
+        Serial.print(" - ");
+        Serial.write(c);
+        Serial.println();
+        
+    } 
+    
+}
